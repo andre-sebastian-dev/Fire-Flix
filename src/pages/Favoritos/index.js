9ca179c6642adf171d14f1c4b368eb37318f1852
andredev <andresebastian056@gmail.com>
2026-04-01 17:06:45 -0300

    Atualização do layout de página favoritos, adição da funcionalidade de excluir itens da lista de favoritos, redimensionalização das capas da pagina home, correção da posição do header

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,51 @@
+import "./style.css"
+import { useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 
 
 function Favoritos(){
+    const [filmes,setFilmes] = useState([])
+
+    useEffect(()=>{
+        const minhaLista = localStorage .getItem("@favorito");
+        setFilmes(JSON.parse(minhaLista) || []);
+    },[])
+
+    function excluirFilme(id){
+        let filtrofilmes = filmes.filter((item)=>{ 
+            return( item.id !== id)
+        })
+        setFilmes(filtrofilmes);
+        localStorage.setItem("@favorito", JSON.stringify(filtrofilmes))
+    }
+
+
+
     return(
-        <div>Favoritos</div>
+        <div className="container">
+            <h1 className="title-f">Meus Favoritos</h1>
+
+            {filmes.length === 0 && <span> Você não tem nenhum filme salvo ; (</span>}
+
+            <ul className="ul">
+                {filmes.map((item)=>{
+                    return(
+                        <div className="hud">
+                        <Link to={`/sobre/${item.id}`} className="posicao">
+
+                            <img key={item.id} src={`http://image.tmdb.org/t/p/original/${item.poster_path}`}/>
+                            <div className="li">
+                                <li className="bold" key={item.id}>{item.title}</li>
+                                <li key={item.id}>{item.overview}</li>
+                            </div>
+                        </Link>
+                                <button onClick={()=> excluirFilme(item.id) } className="excluir">excluir</button>
+                        </div>
+                        
+                    )
+                })}
+            </ul>
+        </div>
     );
 }
 
